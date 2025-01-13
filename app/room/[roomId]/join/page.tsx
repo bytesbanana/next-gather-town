299fd46985bf511731b17a5875d758487d68c5e7
bytesbanana <saharat.paynok@gmail.com>
2025-01-13 22:12:20 +0700

    feat: :sparkles: add join room page

---
+++ app/room/[roomId]/join/page.tsx
@@ -0,0 +1,28 @@
+import { Button } from "@/components/ui/button";
+
+import { Label } from "@/components/ui/label";
+import { ArrowBigLeft } from "lucide-react";
+import Link from "next/link";
+import JoinRoomForm from "@/components/form/JoinRoomForm";
+
+const JoinRoom = ({ params }: { params: { roomId: string } }) => {
+  const { roomId } = params;
+
+  return (
+    <div className="container mx-auto flex h-screen w-screen items-center justify-center p-8">
+      <div className="absolute left-4 top-4 p-4 transition hover:scale-110 hover:cursor-pointer">
+        <Button asChild>
+          <Link href={`/`} className="inline-flex items-center">
+            <ArrowBigLeft />
+            <Label htmlFor="picture" className="font-semibold">
+              Back to home
+            </Label>
+          </Link>
+        </Button>
+      </div>
+      <JoinRoomForm roomId={roomId} />
+    </div>
+  );
+};
+
+export default JoinRoom;
