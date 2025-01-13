6552bb383dc53b14c6a4f1abf2c591f0a030c616
bytesbanana <saharat.paynok@gmail.com>
2025-01-13 22:08:14 +0700

    feat: :sparkles: add chart room card component

--- components/card/chat-room-card.tsx
+++ components/card/chat-room-card.tsx
@@ -2,6 +2,7 @@ import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Users } from "lucide-react";
 import { ChatRoom } from "@/data/chatRooms";
+import Link from "next/link";
 
 interface ChatRoomCardProps {
   room: ChatRoom;
@@ -13,14 +14,16 @@ export function ChatRoomCard({ room }: ChatRoomCardProps) {
       <CardHeader>
         <CardTitle>{room.name}</CardTitle>
       </CardHeader>
-      <CardContent className="flex justify-between items-center">
+      <CardContent className="flex items-center justify-between">
         <div className="flex items-center space-x-2">
           <Users size={20} />
           <span>
             {room.participants} / {room.maxParticipants}
           </span>
         </div>
-        <Button>Join Room</Button>
+        <Button asChild>
+          <Link href={`/room/${room.id}/join`}>Join Room</Link>
+        </Button>
       </CardContent>
     </Card>
   );
