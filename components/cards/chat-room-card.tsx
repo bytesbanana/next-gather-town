d0aa1cd443430db8a9b5621a9809f158e9f50834
bytesbanana <saharat.paynok@gmail.com>
2025-01-14 22:52:29 +0700

    feat: :sparkles: add chat room card

---
+++ components/cards/chat-room-card.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Users } from "lucide-react";
+import { ChatRoom } from "@/data/chatRooms";
+import Link from "next/link";
+
+interface ChatRoomCardProps {
+  room: ChatRoom;
+}
+
+export function ChatRoomCard({ room }: ChatRoomCardProps) {
+  return (
+    <Card className="w-full">
+      <CardHeader>
+        <CardTitle>{room.name}</CardTitle>
+      </CardHeader>
+      <CardContent className="flex justify-between items-center">
+        <div className="flex items-center space-x-2">
+          <Users size={20} />
+          <span>
+            {room.participants} / {room.maxParticipants}
+          </span>
+        </div>
+        <Button asChild>
+          <Link href={`/room/${room.id}/join`}>Join Room</Link>
+        </Button>
+      </CardContent>
+    </Card>
+  );
+}
