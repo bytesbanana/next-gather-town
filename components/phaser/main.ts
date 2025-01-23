c446e61ceaff1ef9374c0c2fcecdd5ede5910861
bytesbanana <saharat.paynok@gmail.com>
2025-01-23 21:56:54 +0700

    feat: :sparkles: add physics config

--- components/phaser/main.ts
+++ components/phaser/main.ts
@@ -1,12 +1,22 @@
 import * as Phaser from "phaser";
 import { Preloader } from "./scenes/Preloader";
+import { Game } from "./scenes/Game";
 
 const config = {
   type: Phaser.AUTO,
   width: 1024,
   height: 768,
   parent: "phaser-container",
-  scene: [Preloader],
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: {
+        y: 0,
+        x: 0,
+      },
+    },
+  },
+  scene: [Preloader, Game],
 };
 
 const StartGame = () => {
