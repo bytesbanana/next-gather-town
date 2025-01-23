d6b0edb3aa2931de3c0f1c3a005486efadc2f837
bytesbanana <saharat.paynok@gmail.com>
2025-01-23 22:00:24 +0700

    style: :lipstick: center phaser container

--- components/phaser/PhaserGame.tsx
+++ components/phaser/PhaserGame.tsx
@@ -26,7 +26,9 @@ const PhaserConatiner = () => {
     <div
       id="phaser-container"
       ref={containerRef}
-      className={cn("absolute left-[50%] -translate-x-1/2")}
+      className={cn(
+        "absolute left-[50%] top-[50%] -translate-x-1/2 -translate-y-1/2",
+      )}
     ></div>
   );
 };
